295298ad44e5782a222422147181087f56f82ebf
Dalton <61704761+daltonhardt@users.noreply.github.com>
2025-09-18 17:10:30 +0200

    Update Facturas.py

--- Facturas.py
+++ Facturas.py
@@ -835,7 +835,7 @@ if tab == TAB_3:  # Create NEW Invoice
                     with grid[0]:
                         line = st.text_input('Descripción *', value='', placeholder='', key=f'description{row}')
                     with grid[1]:
-                        line_qty = st.number_input('Cant.', min_value=0.01, key=f'qty{row}')
+                        line_qty = st.number_input('Cant.', min_value=0.01, value=1, key=f'qty{row}')
                     with grid[2]:
                         line_value = st.number_input('Val.unit.', format="%0.2f", key=f'value{row}')
                     with grid[3]:
